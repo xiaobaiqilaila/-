f754fd6c646f030ff4814bfd21a90bff52766cc7
xiaobaiqilaila <xiaobaiqialila@163.com>
2026-04-16 14:10:47 +0800

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,29 @@
+//ÍêÈ«±³°ü 
+#include<stdio.h>
+#include<stdlib.h>
+
+int max(int a,int b){
+	return a>b?a:b;
+}
+
+int main(){
+	int cap,count;
+	scanf("%d%d",&cap,&count);
+	int *weight,*value,*dp;
+	weight=(int*)malloc(sizeof(int)*count); 
+	value=(int*)malloc(sizeof(int)*count);
+	dp=(int*)calloc((cap+1),sizeof(int));
+	for(int i=0;i<count;i++) scanf("%d",&weight[i]);
+	for(int i=0;i<count;i++) scanf("%d",&value[i]);
+	for(int i=0;i<count;i++){
+		for(int j=1;j<=cap;j++){
+			if(j>=weight[i]){
+				dp[j]=max(dp[j],dp[j-weight[i]]+value[i]);
+			}
+		}
+	}
+	printf("%d",dp[cap]);
+	free(weight);
+	free(value);
+	free(dp);
+}
