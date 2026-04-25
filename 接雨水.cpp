f18e4b4624c0fee036bfff4623f9ec269a1db78e
xiaobaiqilaila <xiaobaiqialila@163.com>
2026-04-25 16:58:55 +0800

    Update 接雨水.cpp

--- 接雨水.cpp
+++ 接雨水.cpp
@@ -1,7 +1,10 @@
-//½ÓÓêË®
+//æ¥é›¨æ°´
 #include<stdio.h>
 #include<stdlib.h>
+#define get(a) printf("%d\n",a)
+#define action 2
 
+#if action==1
 int main(){
 	int n,*num,*item,maxindex=0,ans=0;
 	scanf("%d",&n);
@@ -25,3 +28,32 @@ int main(){
 	free(item);
 	return 0;
 }
+#endif
+
+#if action==2
+int min(int a,int b){
+	return a<b?a:b;
+}
+int main(){
+	int n,*num,*stack,tail=0,ans=0;
+	scanf("%d",&n);
+	num=(int*)malloc(sizeof(int)*n);
+	stack=(int*)malloc(sizeof(int)*n);
+	stack[0]=0;
+	for(int i=0;i<n;i++){
+		scanf("%d",&num[i]);
+	}
+	for(int i=1;i<n;i++){
+		while(num[i]>num[stack[tail]]){
+			tail--;
+			if (tail<0) break;
+			ans+=(min(num[i],num[stack[tail]])-num[stack[tail+1]])*(i-stack[tail]-1);
+		}
+		stack[++tail]=i;
+	}
+	printf("%d",ans);
+	free(num);
+	free(stack);
+	return 0;
+}
+#endif
